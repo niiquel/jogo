f3ac85209f83cb1f6044d44b3f0a3c4611601a7d
NicolasASC <nicolas.ads@aluno.ifsc.edu.br>
2022-02-10 23:49:11 +0000

    sprite2 correção

--- cliente/index.js
+++ cliente/index.js
@@ -6,7 +6,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
-            debug: true
+            debug: false
         }
     },
     scene: {
@@ -16,8 +16,8 @@ const config = {
     }
 };
 
-// var player1;
-// var player2;
+ var player1
+ var player2
  var cursors
  var gameOver = false;
 // var scoreText;
@@ -33,6 +33,11 @@ function preload ()
         frameWidth: 15,
         frameHeight: 16,
       });
+
+    this.load.spritesheet("player2", "./assets/sprite2.png", {
+        frameWidth: 15,
+        frameHeight: 16,
+      });
 }
 
 function create ()
@@ -45,13 +50,13 @@ function create ()
     const belowLayer2 = map.createLayer('belowlayer2', tileset, 0, 0);
     const worldLayer = map.createLayer('worldlayer', tileset, 0, 0);
     
-    worldLayer.setCollisionByProperty({ collides: true })
+    worldLayer.setCollisionByProperty({ collides: true });
 
-    player1 = this.physics.add.sprite(384, 768, 'player1', 0)
-    //player2 = this.physics.add.sprite(736, 48, 'player2')
+    player1 = this.physics.add.sprite(400, 768, 'player1', 0);
+    player2 = this.physics.add.sprite(752, 48, 'player2', 0);
 
-    
     player1.setCollideWorldBounds(true);
+    player2.setCollideWorldBounds(true);
 
     this.anims.create({
         key: "left1",
@@ -105,6 +110,58 @@ function create ()
 
     this.physics.add.collider(player1, worldLayer, null, null, this);
 
+    this.anims.create({
+      key: "left2",
+      frames: this.anims.generateFrameNumbers("player2", {
+        start: 3,
+        end: 5,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+  this.anims.create({
+      key: "right2",
+      frames: this.anims.generateFrameNumbers("player2", {
+        start: 6,
+        end: 8,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+  this.anims.create({
+      key: "stopped2",
+      frames: this.anims.generateFrameNumbers("player2", {
+        start: 0,
+        end: 1,
+      }),
+      frameRate: 2,
+      repeat: -1,
+    });
+
+  this.anims.create({
+      key: "up2",
+      frames: this.anims.generateFrameNumbers("player2", {
+        start: 9,
+        end: 11,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "down2",
+      frames: this.anims.generateFrameNumbers("player2", {
+        start: 0,
+        end: 2,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.physics.add.collider(player2, worldLayer, null, null, this);
+
     cursors = this.input.keyboard.createCursorKeys();
 
     
@@ -116,59 +173,42 @@ function create ()
 function update ()
 {
     if (cursors.left.isDown) {
-        player1.body.setVelocityX(-100);
+        player1.body.setVelocityX(-50);
         player1.anims.play("left1", true);
       } else if (cursors.right.isDown) {
-        player1.body.setVelocityX(100);
+        player1.body.setVelocityX(50);
         player1.anims.play("right1", true);
       } else {
         player1.body.setVelocity(0);
         player1.anims.play("stopped1", true);
       }
-      if (cursors.up.isDown) {
-        player1.body.setVelocityY(-100);
+    if (cursors.up.isDown) {
+        player1.body.setVelocityY(-50);
+        player1.anims.play("up1", true);
       } else if (cursors.down.isDown) {
-        player1.body.setVelocityY(100);
+        player1.body.setVelocityY(50);
+        player1.anims.play("down1", true);
       } else {
         player1.body.setVelocityY(0);
       }
-}
 
-function collectStar (player, star)
-{
-    star.disableBody(true, true);
-
-    //  Add and update the score
-    score += 10;
-    scoreText.setText('Score: ' + score);
-
-    if (stars.countActive(true) === 0)
-    {
-        //  A new batch of stars to collect
-        stars.children.iterate(function (child) {
-
-            child.enableBody(true, child.x, 0, true, true);
-
-        });
-
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bomb.allowGravity = false;
-
-    }
-}
-
-function hitBomb (player, bomb)
-{
-    this.physics.pause();
-
-    player.setTint(0xff0000);
-
-    player.anims.play('turn');
-
-    gameOver = true;
+      if (cursors.left.isDown) {
+        player2.body.setVelocityX(-50);
+        player2.anims.play("left2", true);
+      } else if (cursors.right.isDown) {
+        player2.body.setVelocityX(50);
+        player2.anims.play("right2", true);
+      } else {
+        player2.body.setVelocity(0);
+        player2.anims.play("stopped2", true);
+      }
+    if (cursors.up.isDown) {
+        player2.body.setVelocityY(-50);
+        player2.anims.play("up2", true);
+      } else if (cursors.down.isDown) {
+        player2.body.setVelocityY(50);
+        player2.anims.play("down2", true);
+      } else {
+        player2.body.setVelocityY(0);
+      }
 }
